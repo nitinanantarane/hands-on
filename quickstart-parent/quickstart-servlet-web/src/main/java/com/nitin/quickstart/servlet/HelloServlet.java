6d7afe882c547194038e4c58f32be3b49b6790fa
nitinrane <nitinanantarane@gmail.com>
2019-12-10 15:51:09 -0500

    Initial commit for quickstart-servlet-web

---
+++ quickstart-parent/quickstart-servlet-web/src/main/java/com/nitin/quickstart/servlet/HelloServlet.java
@@ -0,0 +1,26 @@
+package com.nitin.quickstart.servlet;
+
+import java.io.IOException;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+public class HelloServlet extends HttpServlet {
+
+	private static final long serialVersionUID = -1613957758611985714L;
+	
+	@Override
+	protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+		doPost(req, resp);
+	}
+	
+	@Override
+	protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+		resp.setContentType("text/html");
+		resp.getOutputStream().println("<h2>Hello " + System.getProperty("user.name"));
+		resp.flushBuffer();
+	}
+
+}
