e10c6259fd94f2be73c572e295b2483f78b7cc64
nitinrane <nitinanantarane@gmail.com>
2019-12-22 10:16:04 -0500

    kafka examples

--- quickstart-parent/quickstart-kafka/src/main/java/com/nitin/quickstart/kafka/WordCountProcessorDemo.java
+++ quickstart-parent/quickstart-kafka/src/main/java/com/nitin/quickstart/kafka/WordCountProcessorDemo.java
@@ -1,19 +1,3 @@
-/*
- * Licensed to the Apache Software Foundation (ASF) under one or more
- * contributor license agreements. See the NOTICE file distributed with
- * this work for additional information regarding copyright ownership.
- * The ASF licenses this file to You under the Apache License, Version 2.0
- * (the "License"); you may not use this file except in compliance with
- * the License. You may obtain a copy of the License at
- *
- *    http://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
- */
 package com.nitin.quickstart.kafka;
 
 import java.time.Duration;
@@ -35,21 +19,6 @@ import java.util.Locale;
 import java.util.Properties;
 import java.util.concurrent.CountDownLatch;
 
-/**
- * Demonstrates, using the low-level Processor APIs, how to implement the WordCount program
- * that computes a simple word occurrence histogram from an input text.
- * <p>
- * <strong>Note: This is simplified code that only works correctly for single partition input topics.
- * Check out {@link WordCountDemo} for a generic example.</strong>
- * <p>
- * In this example, the input stream reads from a topic named "streams-plaintext-input", where the values of messages
- * represent lines of text; and the histogram output is written to topic "streams-wordcount-processor-output" where each record
- * is an updated count of a single word.
- * <p>
- * Before running this example you must create the input topic and the output topic (e.g. via
- * {@code bin/kafka-topics.sh --create ...}), and write some data to the input topic (e.g. via
- * {@code bin/kafka-console-producer.sh}). Otherwise you won't see any data arriving in the output topic.
- */
 public final class WordCountProcessorDemo {
 
     static class MyProcessorSupplier implements ProcessorSupplier<String, String> {
